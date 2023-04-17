b8d6beee27dc759dc3f4bdb026311778e35a7c93
luong_cuong <luongquoccuong196@gmail.com>
2023-04-17 13:51:45 +0700

    run again fact_sales_order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -3,5 +3,4 @@ order_line_id as sales_order_line_key
 , quantity as quantity
 , unit_price as unit_price
 , quantity * unit_price as gross_amount
-, stock_item_id as product_key
  FROM `vit-lam-data.wide_world_importers.sales__order_lines` 
