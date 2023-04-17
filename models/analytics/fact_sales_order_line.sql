e414a76889b309a25056b6b6a8394b252f2ddcb4
luong_cuong <luongquoccuong196@gmail.com>
2023-04-17 14:01:09 +0700

    update

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,7 @@
 SELECT 
-order_line_id as sales_order_line_key
+ stock_item_id as product_key
+, order_line_id as sales_order_line_key
 , quantity as quantity
 , unit_price as unit_price
 , quantity * unit_price as gross_amount
-, stock_item_id as product_key
  FROM `vit-lam-data.wide_world_importers.sales__order_lines` 
