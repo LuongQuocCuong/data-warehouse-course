883ad3e2d616ceeb07656d5a9ea9bc5c023e04a4
luong_cuong <luongquoccuong196@gmail.com>
2023-04-18 09:28:40 +0700

    update customer_key to fact_sales_order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,6 +1,8 @@
 with fact_sale__soure as (
 select * 
-from `vit-lam-data.wide_world_importers.sales__order_lines`
+from `vit-lam-data.wide_world_importers.sales__order_lines` as order_lines
+inner join `vit-lam-data.wide_world_importers.sales__orders` as orders
+on orders.order_id = order_lines.order_id
 )
 
 , fact_sale_rename_column as (
@@ -9,6 +11,7 @@ select
   ,order_line_id AS sales_order_line_key
   ,quantity as quantity
   ,unit_price as unit_price
+  ,customer_id as customer_key
 from fact_sale__soure
 )
 
@@ -18,6 +21,7 @@ select
   ,cast (sales_order_line_key as integer) as sales_order_line_key
   ,cast (quantity as integer) as quantity
   ,cast (unit_price as numeric) as unit_price
+  ,cast (customer_key as integer) as customer_key
 from fact_sale_rename_column
 )
 
@@ -26,5 +30,6 @@ SELECT
 , sales_order_line_key
 , quantity
 , unit_price
+, customer_key
 ,quantity * unit_price as gross_amount
  FROM fact_sale_cast_data
