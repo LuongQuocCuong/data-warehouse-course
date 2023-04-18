deafd5e764ced206f595587776185b069e7e23ff
luong_cuong <luongquoccuong196@gmail.com>
2023-04-18 08:50:56 +0700

    fomat data tupe dim_customer

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,4 +1,23 @@
+with dim_customer__source as (
+select * 
+from `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_customer__rename_coulumn as (
+select
+  customer_id as customer_key
+  ,customer_name as customer_name
+from dim_customer__source
+)
+
+, dim_customer__cast_data as(
+select  
+  cast(customer_key as integer) as customer_key
+  ,cast (customer_name as string) as customer_name
+from dim_customer__rename_coulumn
+)
+
 SELECT 
-customer_id as customer_key
-, customer_name as customer_name
- FROM `vit-lam-data.wide_world_importers.sales__customers`
+customer_key
+,customer_name
+ FROM dim_customer__cast_data
