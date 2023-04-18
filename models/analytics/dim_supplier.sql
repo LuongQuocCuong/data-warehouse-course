c78e88a5613482bebf2429ad0ea245de2b1fbc4c
luong_cuong <luongquoccuong196@gmail.com>
2023-04-18 15:58:23 +0700

    create dim supplier

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,24 @@
+with dim_supplier__source as (
 SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, dim_supplier__rename_column as (
+SELECT 
+   supplier_id as supplier_key
+  ,supplier_name	as supplier_name
+FROM dim_supplier__source
+)
+
+, dim_supplier__cast_data as (
+SELECT
+  cast(supplier_key as integer) as supplier_key
+  ,cast (supplier_name as string) as supplier_name
+FROM dim_supplier__rename_column
+)
+
+select 
+  supplier_key
+  , supplier_name
+FROM dim_supplier__cast_data
