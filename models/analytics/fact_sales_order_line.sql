6b51a5b080e9fffdd8e74aa085e340bf3d0b78a1
luong_cuong <luongquoccuong196@gmail.com>
2023-04-18 08:41:20 +0700

    fomat code fact sale_order

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,30 @@
+with fact_sale__soure as (
+select * 
+from `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+, fact_sale_rename_column as (
+select
+  stock_item_id as product_key
+  ,order_line_id AS sales_order_line_key
+  ,quantity as quantity
+  ,unit_price as unit_price
+from fact_sale__soure
+)
+
+, fact_sale_cast_data as (
+select
+  cast (product_key as integer) as product_key
+  ,cast (sales_order_line_key as integer) as sales_order_line_key
+  ,cast (quantity as integer) as quantity
+  ,cast (unit_price as numeric) as unit_price
+from fact_sale_rename_column
+)
+
 SELECT 
- cast (stock_item_id as integer) as product_key
-, cast (order_line_id as integer) as sales_order_line_key
-, cast (quantity as integer) as quantity
-, cast (unit_price as numeric) as unit_price
-, cast(quantity as integer) * cast(unit_price as numeric ) as gross_amount
- FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+  product_key
+, sales_order_line_key
+, quantity
+, unit_price
+,quantity * unit_price as gross_amount
+ FROM fact_sale_cast_data
