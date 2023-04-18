bb0b435da3c9087a6c8af51ad2f58b5099acd17d
luong_cuong <luongquoccuong196@gmail.com>
2023-04-18 18:11:18 +0700

    flatten dim product add column: (supllier_id, supplier name)

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -8,6 +8,7 @@ select
   stock_item_id as product_key
   ,stock_item_name as product_name
   ,brand as brand_name
+  ,supplier_id as supplier_key
 from dim_product__source
 )
 
@@ -16,11 +17,16 @@ select
   CAST(product_key AS INTEGER) as product_key
   , CAST(product_name AS STRING) as product_name
   , CAST(brand_name AS STRING) as brand_name
+  , CAST(supplier_key AS INTEGER) AS supplier_key
 from dim_product__rename_column
 )
 
 SELECT 
-  product_key
-  , product_name
-  , brand_name
-FROM dim_product__cast_type
+  dim_product.product_key
+  , dim_product.product_name
+  , dim_product.brand_name
+  , dim_product.supplier_key
+  , dim_supplier.supplier_name
+FROM dim_product__cast_type as dim_product
+LEFT JOIN {{ref('dim_supplier')}} as dim_supplier
+on dim_product.supplier_key = dim_supplier.supplier_key
