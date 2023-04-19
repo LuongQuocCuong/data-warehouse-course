95cdb376238b23618c5ff77e56a8e1f169eff692
luong_cuong <luongquoccuong196@gmail.com>
2023-04-19 16:38:05 +0700

    update coumn on credit hold

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -9,6 +9,7 @@ select
   ,customer_name as customer_name
   ,customer_category_id as customer_category_key
   ,buying_group_id as buying_group_key
+  ,is_on_credit_hold as is_on_credit_hold
 from dim_customer__source
 )
 
@@ -18,7 +19,20 @@ select
   ,cast (customer_name as string) as customer_name
   ,cast(customer_category_key as integer ) as customer_category_key
   ,cast(buying_group_key as integer) as buying_group_key
+  ,cast (is_on_credit_hold as boolean) as is_on_credit_hold_boolean
 from dim_customer__rename_coulumn
+)
+
+, dim_customer__convert_is_on_credit_hold as(
+SELECT
+  *
+  , case 
+      when is_on_credit_hold_boolean is true then 'On Hold Credit'
+      when is_on_credit_hold_boolean is false then 'Not On Hold Credit'
+      when is_on_credit_hold_boolean is null then 'Undefined'
+      else 'invalid'
+    END AS is_on_credit_hold
+FROM dim_customer__cast_data
 )
 
 SELECT 
@@ -28,7 +42,8 @@ dim_customer.customer_key
 ,customer_categories.customer_category_name
 ,dim_customer.buying_group_key
 ,buying_group.buying_group_name
-FROM dim_customer__cast_data as dim_customer
+,dim_customer.is_on_credit_hold
+FROM dim_customer__convert_is_on_credit_hold as dim_customer
 left join {{ref('stg_customer_categories')}} as customer_categories
 on customer_categories.customer_category_key = dim_customer.customer_category_key
 left join {{ref('buying_group')}} as buying_group
