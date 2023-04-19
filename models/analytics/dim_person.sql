6cef2bcc3909fcbaa4a4470ba061c75bb7ba33a7
luong_cuong <luongquoccuong196@gmail.com>
2023-04-19 21:25:06 +0700

    create dim_person

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,23 @@
-SELECT 
-  *
+with dim_person__source as (
+SELECT *
 FROM `vit-lam-data.wide_world_importers.application__people`
+)
+
+, dim_person__rename as (
+SELECT 
+  person_id as person_key
+  , full_name
+FROM dim_person__source
+)
+
+, dim_person__cast_data as(
+SELECT
+  cast (person_key as integer) as person_key
+  ,cast(full_name as string) as full_name
+FROM dim_person__rename
+)
+
+SELECT 
+  person_key
+  ,coalesce (full_name, 'Undifined') as full_name
+FROM dim_person__cast_data
