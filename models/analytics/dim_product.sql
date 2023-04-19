c56fe031004a8e447ff8898e01a19d365ae8d01a
luong_cuong <luongquoccuong196@gmail.com>
2023-04-19 16:03:50 +0700

    convert chiller stock boolean

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -19,8 +19,20 @@ select
   , CAST(product_name AS STRING) as product_name
   , CAST(brand_name AS STRING) as brand_name
   , CAST(supplier_key AS INTEGER) AS supplier_key
-  , CAST (is_chiller_stock as boolean ) as is_chiller_stock
+  , CAST (is_chiller_stock as boolean ) as is_chiller_stock_boolean
 from dim_product__rename_column
+)
+
+, dim_product__convert_is_chiller_stock as (
+  select 
+    *,
+    case
+      when is_chiller_stock_boolean is true then 'Chiller Stock'
+      when is_chiller_stock_boolean is false then 'Not Chiller Stock'
+      when is_chiller_stock_boolean is null then 'undefined'
+      else 'invalid'
+    end as is_chiller_stock
+  from dim_product__cast_type
 )
 
 SELECT 
@@ -30,6 +42,6 @@ SELECT
   , dim_product.supplier_key
   , dim_supplier.supplier_name
   , dim_product.is_chiller_stock
-FROM dim_product__cast_type as dim_product
+FROM dim_product__convert_is_chiller_stock as dim_product
 LEFT JOIN {{ref('dim_supplier')}} as dim_supplier
 on dim_product.supplier_key = dim_supplier.supplier_key
