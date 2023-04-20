e99a9b8aaa43ef87abefee6d0edb178e7a211a85
luong_cuong <luongquoccuong196@gmail.com>
2023-04-20 09:19:10 +0700

    update person pickup key

--- models/staging/stg_sale_order.sql
+++ models/staging/stg_sale_order.sql
@@ -7,6 +7,7 @@ from `vit-lam-data.wide_world_importers.sales__orders`
 select
   customer_id as customer_key
   ,order_id as sale_order_key
+  ,picked_by_person_id as picked_by_person_key
 from stg_sale_order__source
 )
 
@@ -14,10 +15,12 @@ from stg_sale_order__source
 select
   cast(customer_key as integer) as customer_key
   , cast(sale_order_key as integer) as sale_order_key
+  , cast (picked_by_person_key as integer) as picked_by_person_key
 from stg_sale_order__rename
 )
 
 SELECT 
   customer_key
   ,sale_order_key
+  ,picked_by_person_key
 FROM stg_sale_order__cast_data
