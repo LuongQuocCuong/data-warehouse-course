fb31d5294b060782ac800cde9e27e4d11f038211
luong_cuong <luongquoccuong196@gmail.com>
2023-04-25 19:09:30 +0700

    update dim_supplier

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -8,17 +8,31 @@ FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
 SELECT 
    supplier_id as supplier_key
   ,supplier_name	as supplier_name
+  ,supplier_category_id as supplier_category_key
+  ,delivery_city_id as delivery_city_key
 FROM dim_supplier__source
 )
 
 , dim_supplier__cast_data as (
 SELECT
   cast(supplier_key as integer) as supplier_key
-  ,cast (supplier_name as string) as supplier_name
+  , cast (supplier_name as string) as supplier_name
+  , cast (supplier_category_key as integer) as supplier_category_key
+  , cast (delivery_city_key as integer) as delivery_city_key
 FROM dim_supplier__rename_column
 )
 
 select 
-  supplier_key
-  , supplier_name
-FROM dim_supplier__cast_data
+  dim_supllier.supplier_key
+  , dim_supllier.supplier_name
+  , dim_supllier.supplier_category_key
+  , supplier_category.supplier_category_name
+  , dim_supllier.delivery_city_key
+  , stg_location.delivery_city_name
+  , stg_location.delivery_province_name
+  , stg_location.delivery_country_name
+FROM dim_supplier__cast_data as dim_supllier
+LEFT JOIN {{ref('stg_supplier_category')}} as supplier_category
+  ON dim_supllier.supplier_category_key = supplier_category.supplier_category_key
+LEFT JOIN {{ref('stg_location')}} as stg_location
+  ON stg_location.delivery_city_key = dim_supllier.delivery_city_key
