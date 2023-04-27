c469d440f1c89a788cc895ad2e4f2f599ea31758
luong_cuong <luongquoccuong196@gmail.com>
2023-04-27 10:56:23 +0700

    update supplier_category

--- models/staging/stg_supplier_category.sql
+++ models/staging/stg_supplier_category.sql
@@ -18,7 +18,22 @@ SELECT
 FROM supplier_category__rename_column
 )
 
-select 
+, supplier_category__add_undifined_record as (
+SELECT 
   supplier_category_key
   , supplier_category_name
 FROM supplier_category__cast_data
+UNION ALL 
+  SELECT
+    0 AS supplier_category_key
+    , 'Undefined' AS supplier_category_name
+, UNION ALL
+  SELECT
+    -1 AS supplier_category_key
+    , 'Invalid' AS supplier_category_name
+)
+
+select 
+  supplier_category_key
+  , supplier_category_name
+FROM supplier_category__add_undifined_record
