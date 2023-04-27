71902509715ca72dd137934a9f1cbf020e388e76
luong_cuong <luongquoccuong196@gmail.com>
2023-04-27 16:45:33 +0700

    create buying group

--- models/staging/stg_buying_group.sql
+++ models/staging/stg_buying_group.sql
@@ -18,7 +18,22 @@ select
 from buying_group__source__rename_column
 )
 
+,buying_group__add_undefined_record as (
+SELECT 
+  buying_group_key
+  , buying_group_name
+FROM buying_group__cast_type
+UNION ALL
+  SELECT 
+    0 as buying_group_key
+    , 'Undefined' as buying_group_name
+, UNION ALL
+  SELECT
+    -1 as buying_group_key
+    , 'Invalid' as buying_group_name
+)
+
 select
   buying_group_key
   ,buying_group_name
-from buying_group__cast_type
+from buying_group__add_undefined_record
