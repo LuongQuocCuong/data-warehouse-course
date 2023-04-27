54e2467e93327b1d3a354a8667de1d2a1377e3a4
luong_cuong <luongquoccuong196@gmail.com>
2023-04-27 10:08:37 +0700

    update coloumn name supplier_delivery_city_key

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -9,7 +9,7 @@ SELECT
    supplier_id as supplier_key
   ,supplier_name	as supplier_name
   ,supplier_category_id as supplier_category_key
-  ,delivery_city_id as delivery_city_key
+  ,delivery_city_id as supplier_delivery_city_key
 FROM dim_supplier__source
 )
 
@@ -18,7 +18,7 @@ SELECT
   cast(supplier_key as integer) as supplier_key
   , cast (supplier_name as string) as supplier_name
   , cast (supplier_category_key as integer) as supplier_category_key
-  , cast (delivery_city_key as integer) as delivery_city_key
+  , cast (supplier_delivery_city_key as integer) as supplier_delivery_city_key
 FROM dim_supplier__rename_column
 )
 
@@ -27,7 +27,7 @@ select
   , dim_supllier.supplier_name
   , dim_supllier.supplier_category_key
   , supplier_category.supplier_category_name
-  , dim_supllier.delivery_city_key
+  , dim_supllier.supplier_delivery_city_key
   , stg_location.supplier_delivery_city_name
   , stg_location.supplier_delivery_province_name
   , stg_location.supplier_delivery_country_name
@@ -36,4 +36,4 @@ FROM dim_supplier__cast_data as dim_supllier
 LEFT JOIN {{ref('stg_supplier_category')}} as supplier_category
   ON dim_supllier.supplier_category_key = supplier_category.supplier_category_key
 LEFT JOIN {{ref('stg_location')}} as stg_location
-  ON stg_location.supplier_delivery_city_key = dim_supllier.delivery_city_key
+  ON stg_location.supplier_delivery_city_key = dim_supllier.supplier_delivery_city_key
