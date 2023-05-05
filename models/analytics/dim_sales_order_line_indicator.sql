520dd047f98311cd8b60cea8cb2d1c9d9f04c166
luong_cuong <luongquoccuong196@gmail.com>
2023-05-05 09:53:10 +0700

    add undefined record

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -1,8 +1,38 @@
-WITH compile_table as (
+WITH indicator_table__source as (
 SELECT * FROM `data-warehouse-course-384003.wide_world_importers_dwh_staging.stg_undersupply_back_ordered` 
   CROSS JOIN `data-warehouse-course-384003.wide_world_importers_dwh_staging.stg_package_type`
 )
+
+, indicator_table__add_column_key AS (
 SELECT
   FARM_FINGERPRINT(CONCAT(is_undersupply_back_ordered_key,"-",package_type_key)) as sales_order_line_indicator_key
-  ,*
-FROM compile_table
+  , is_undersupply_back_ordered_name
+  , package_type_key
+  , package_type_name
+FROM indicator_table__source
+)
+
+, indicator_table__add_undifined_record AS (
+SELECT
+  sales_order_line_indicator_key
+  , is_undersupply_back_ordered_name
+  , package_type_key
+  , package_type_name
+FROM indicator_table__add_column_key
+UNION ALL
+  SELECT
+    0 AS sales_order_line_indicator_key
+    , 'Undefined' AS is_undersupply_back_ordered_name
+    , 0 AS package_type_key
+    , 'Undefined' AS package_type_name
+, UNION ALL
+  SELECT
+    -1 AS sales_order_line_indicator_key
+    , 'Invalid' AS is_undersupply_back_ordered_name
+    , -1 AS package_type_key
+    , 'Invalid' AS package_type_name
+)
+
+SELECT
+*
+FROM indicator_table__add_undifined_record
