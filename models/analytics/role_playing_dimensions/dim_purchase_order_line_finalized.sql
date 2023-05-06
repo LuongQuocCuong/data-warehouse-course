70efb806e1026ad5d0461523e0cbd4be46eb7e2c
luong_cuong <luongquoccuong196@gmail.com>
2023-05-06 12:40:00 +0700

    create table dim_purchase_order_line

---
+++ models/analytics/role_playing_dimensions/dim_purchase_order_line_finalized.sql
@@ -0,0 +1,40 @@
+WITH finalized_indicator_true AS(
+SELECT 
+  'true' AS is_order_line_finalized_boolean
+  ,'Order Finalized' AS is_order_finalized
+)
+, finalized_indicator_false AS(
+SELECT * 
+FROM finalized_indicator_true
+UNION ALL
+  SELECT  
+    'false' AS is_order_line_finalized_boolean
+    , 'Order Not Finalized' AS is_order_finalized
+)
+
+, finalized_indicator_cast_data AS (
+SELECT
+  CAST(FARM_FiNGERPRINT(is_order_line_finalized_boolean) AS INTEGER) AS order_line_finalized_key
+  , is_order_finalized AS order_line_finalized_name
+FROM finalized_indicator_false
+)
+
+, finalized_indicator_add_undefined_record AS (
+SELECT
+  order_line_finalized_key
+  , order_line_finalized_name
+FROM finalized_indicator_cast_data
+UNION ALL
+  SELECT
+    0 AS order_line_finalized_key
+    , 'Undefined' AS order_line_finalized_name
+, UNION ALL
+  SELECT
+    -1 AS order_line_finalized_key
+    , 'Invalid' AS order_line_finalized_name
+)
+
+SELECT
+  order_line_finalized_key
+  , order_line_finalized_name
+FROM finalized_indicator_add_undefined_record
